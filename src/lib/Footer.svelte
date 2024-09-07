c568cb911c2198a84a69d64ddbf09f78e8ef3fe0
Leif Doxey-Van Heest <ldoxeyvh@umich.edu>
2024-09-06 23:23:29 -0400

    positioned footer correctly

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,28 +1,20 @@
 <script lang='ts'>
- let { pages = [] } = $props();
+ import type { Page } from '$lib/types'
+
+ interface Props {
+     pages: Page[]
+ }
+
+ let { pages = [] }: Props = $props();
 </script>
 
-<footer>
+<footer class="bg-neutral-50 bottom-0 w-full">
     <div>
         Icons placeholder
     </div>
-    <ul>
+    <ul class="flex flex-row space-x-4 place-content-center mb-10">
         {#each pages as page}
             <li><a href={page.route}>{page.label}</a></li>
         {/each}
     </ul>
 </footer>
-
-<style>
- footer {
-     position: fixed;
-     left: 0;
-     bottom: 0;
-     width: 100%;
- }
-
- li {
-     display: inline-block;
-     margin-right: 5%
- }
-</style>
