63dfb9621220a662b56a551c2231cd76cc5ae881
Leif Doxey-Van Heest <ldoxeyvh@umich.edu>
2025-03-16 18:57:05 -0400

    added new title component

---
+++ src/lib/Title.svelte
@@ -0,0 +1,7 @@
+<script>
+    let { text } = $props();
+</script>
+
+<div class="my-8 grid justify-items-center">
+    <span class="font-bungee-shade font-semibold text-5xl text-lime-700">{text}</span>
+</div>
