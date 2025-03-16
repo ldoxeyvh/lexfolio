cdfebcff38be752603ee63332cd18544c05b1381
Leif Doxey-Van Heest <ldoxeyvh@umich.edu>
2025-03-16 18:58:08 -0400

    updated title components

--- src/routes/about-me/+page.svelte
+++ src/routes/about-me/+page.svelte
@@ -1,12 +1,11 @@
 <script>
     import Popout from "$lib/Popout.svelte";
+    import Title from "$lib/Title.svelte"
     import Placeholder from "$lib/assets/portfolio/mifreshwater.jpg"
 </script>
 
+<Title text="about me"/>
 <div class="grid grid-cols-1 justify-items-center lg:grid-cols-2 gap-5">
-    <div class="my-8 col-span-1 lg:col-span-2 justify-items-center">
-        <span class="font-bungee-shade font-semibold text-5xl text-lime-700">about me</span>
-    </div>
     <div class="rounded-xl">
         <img class="triple-shadow p-4 rounded-xl max-h-96" alt="Alexsey daCosta" src={Placeholder}>
     </div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -4,26 +4,25 @@
     import Botanical from "$lib/assets/portfolio/matthaei_square.png";
     import Environmental from "$lib/assets/portfolio/mifreshwater.jpg";
     import { base } from "$app/paths";
+	import Title from "$lib/Title.svelte";
 
     
 </script>
 
-<div class="my-8 grid justify-items-center">
-    <span class="font-bungee-shade text-5xl text-lime-700 font-semibold">projects</span>
-</div>
+<Title text="projects" />
 <div class="grid grid-cols-2 gap-10 m-4 md:grid md:grid-cols-4">
     <a href="{`${base}/projects/botanical-gardens`}" class="grid grid-rows-2 gap-6 justify-items-center">
         <!-- <div class="p-4 rounded-xl triple-shadow">
             <img src={Botanical} alt="" class="max-w-full max-h-full object-contain">
         </div> -->
-        <TripleShadowImage image={Botanical} alt="" classList=""/>
+        <TripleShadowImage image={Botanical} alt="" classList="" hover=false/>
         <span class="font-league-spartan font-normal text-center">Botanical Gardens Redesign</span>
     </a>
     <a href="{`${base}/projects/environmental-riso`}" class="grid grid-rows-2 gap-6 justify-items-center">
         <!-- <div class="p-4 rounded-xl triple-shadow">
             <img src={Environmental} alt="" class="max-w-full max-h-full object-contain">
         </div> -->
-        <TripleShadowImage image={Environmental} alt="" classList=""/>
+        <TripleShadowImage image={Environmental} alt="" classList="" hover=false/>
         <span class="font-league-spartan font-normal text-center">Enivronmental RISO Project</span>
     </a>
 </div>
