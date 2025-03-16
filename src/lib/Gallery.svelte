ccced87ff3e568c1c86fc347345469ac2f7a262f
Leif Doxey-Van Heest <ldoxeyvh@umich.edu>
2025-03-16 18:55:56 -0400

    made new gallery component

---
+++ src/lib/Gallery.svelte
@@ -0,0 +1,17 @@
+<script>
+    const imageModules = import.meta.glob(
+     '$lib/assets/portfolio/*{.jpg,png,jpeg,gif}',
+        {
+            eager: true,
+            query: {
+                enhanced: true
+            }
+        }
+    )  
+</script>
+
+<div class="columns-2 lg:columns-4 gap-4 space-y-8 m-4">
+    {#each Object.entries(imageModules) as [_path, module]}
+        <enhanced:img class="max-w-1/2 mb-4 p-2 border border-lime-800 rounded-xl" src={module.default} alt="TODO" />
+    {/each}
+</div>
--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,23 +1,8 @@
 <script>
-    import Popout from "$lib/Popout.svelte";
-
-    const imageModules = import.meta.glob(
-     '$lib/assets/portfolio/*{.jpg,png,jpeg,gif}',
-        {
-            eager: true,
-            query: {
-                enhanced: true
-            }
-        }
-    )  
+	import Title from "$lib/Title.svelte";
+    import Gallery from "$lib/Gallery.svelte";
 </script>
 
 
-<div class="grid grid-cols-1 justify-items-center my-8">
-    <span class="font-bungee-shade text-5xl text-lime-700 font-semibold">portfolio</span>
-</div>
-<div class="columns-2 lg:columns-4 gap-4 space-y-8 m-4">
-    {#each Object.entries(imageModules) as [_path, module]}
-        <enhanced:img class="max-w-1/2 mb-4 p-2 border border-lime-800 rounded-xl" src={module.default} alt="TODO" />
-    {/each}
-</div>
+<Title text="portfolio" />
+<Gallery/>
