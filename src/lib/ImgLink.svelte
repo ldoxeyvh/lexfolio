1ef3f54bc775ee89d9806b9cf9d909a08e96b4f7
Leif Doxey-Van Heest <ldoxeyvh@umich.edu>
2025-04-08 15:12:07 -0400

    fixed text sizing bug

--- src/lib/ImgLink.svelte
+++ src/lib/ImgLink.svelte
@@ -5,6 +5,7 @@
     let { text, href, sm, lg, sz="4xl" } = $props();
     let hover = $state(false);
     let captionTracking = $derived(hover ? "tracking-widest" : "tracking-wider");
+    let textSize = $derived(`text-${sz}`);
 </script>
 
 <a href={href} class="grid justify-items-center gap-y-6" 
@@ -14,7 +15,7 @@
     <TripleShadowImage image={lg} alt="" hover={hover} classList="hidden lg:block lg:w-full lg:h-full"/>
     <TripleShadowImage image={sm} alt="" hover={hover} classList="lg:hidden lg:w-full lg:h-full"/>
     <!-- {#if } TODO make this switch between the two types of image -->
-    <span class="font-righteous transition-all {captionTracking} text-lime-700 text-{sz} text-center font-black justify-self-center">
+    <span class="font-righteous transition-all {captionTracking} text-lime-700 {textSize} text-center font-black justify-self-center">
         {text}
     </span>
 </a>
